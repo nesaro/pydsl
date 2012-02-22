8ad805aa9430dab33ccc9eabd735f4ad10d20171
Néstor Arocha Rodríguez <nesaro@colonymbus.com>
2012-02-22 22:03:05 +0000

    Commented non implemented externalmemories

--- ColonyDSL/Config.py
+++ ColonyDSL/Config.py
@@ -33,14 +33,14 @@ def generate_memory_list() -> list:
     from ColonyDSL.Memory.External.DirLibrary.Function import BoardFileLibrary, TransformerDirLibrary, ProcedureFileLibrary
     from ColonyDSL.Memory.External.DictLibrary import FileTypeDictLibrary, RegexpDictLibrary
     from ColonyDSL.Memory.External.ListLibrary import RelListLibrary, RelationListLibrary
-    from ColonyDSL.Memory.External.DirLibrary.Concept import ConceptFileLibrary
-    from ColonyDSL.Memory.External.DirLibrary.Scheme import SchemeFileLibrary
+    #from ColonyDSL.Memory.External.DirLibrary.Concept import ConceptFileLibrary
+    #from ColonyDSL.Memory.External.DirLibrary.Scheme import SchemeFileLibrary
     result.append(GrammarFileLibrary("/usr/share/ColonyDSL/lib_contrib/grammar/"))
     result.append(BoardFileLibrary("/usr/share/ColonyDSL/lib_contrib/board/"))
     result.append(TransformerDirLibrary("/usr/share/ColonyDSL/lib_contrib/transformer/"))
     result.append(ProcedureFileLibrary("/usr/share/ColonyDSL/lib_contrib/procedure/"))
     result.append(FileTypeDictLibrary("/usr/share/ColonyDSL/lib_contrib/dict/filetype.dict"))
-    result.append(SchemeFileLibrary("/usr/share/ColonyDSL/lib_contrib/scheme/"))
+    #result.append(SchemeFileLibrary("/usr/share/ColonyDSL/lib_contrib/scheme/"))
     result.append(RegexpDictLibrary("/usr/share/ColonyDSL/lib_contrib/dict/regexp.dict"))
     return result
 
