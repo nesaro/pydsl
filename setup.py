6d750027a90e44ebc39d5cb626a807b9a8b75601
Nestor Arocha Rodriguez <nesaro@gmail.com>
2012-06-04 11:41:06 +0100

    renamed project: colony -> pydsl

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 import glob
 
-setup(name='ColonyDSL',
+setup(name='pydsl',
       version='0.1',
-      description='?',
+      description='Python Domain Specific Language Tools',
       author='Nestor Arocha',
       author_email='nesaro@gmail.com',
-      url='https://github.com/nesaro/colony',
+      url='https://github.com/nesaro/pydsl',
       packages=['ColonyDSL', 'ColonyDSL/Type',  'ColonyDSL/Type/Grammar/' , 'ColonyDSL/Type/Grammar/Parser/', 'ColonyDSL/Interaction/', 'ColonyDSL/Memory/', 'ColonyDSL/Memory/Storage/', 'ColonyDSL/Memory/Storage/Directory/', 'ColonyDSL/Function/', 'ColonyDSL/Function/Transformer/', 'ColonyDSL/Concept/', 'ColonyDSL/Memory/Search/'],
       scripts=['bin/extract.py', 'bin/translate.py','bin/check.py','bin/search.py', 'bin/guess.py', 'bin/convert.py', 'bin/info.py', 'bin/validate.py', 'bin/parts.py'],
      )
