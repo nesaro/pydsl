043df30f2e0cb6eb42ce998ba720de01f9082cae
Nestor Arocha Rodriguez <nesaro@gmail.com>
2012-08-01 08:42:23 +0100

    added empty SyntaxDirectedTransformer module

---
+++ pydsl/Function/Transformer/SyntaxDirected.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Syntax Directed Transformers"""
+
+import logging
+LOG = logging.getLogger(__name__)
+from .Transformer import Transformer
+
+class SyntaxDirectedTransformer(Transformer):
+    pass
