8486ad505f1c8cecb6ff077125c503483f4602e4
Nestor Arocha <nesaro@gmail.com>
2012-08-13 20:49:36 +0100

    changes on translate program signature

--- bin/translate.py
+++ bin/translate.py
@@ -27,18 +27,17 @@ __email__ = "nesaro@gmail.com"
 import logging
 from pydsl.Exceptions import BadFileFormat
 
-
 if __name__ == "__main__":
     import argparse
-    TUSAGE = "usage: %(prog)s [options] [filename]"
+    TUSAGE = "usage: %(prog)s [options] transformername"
     PARSER = argparse.ArgumentParser(usage = TUSAGE)
     PARSER.add_argument("-d", "--debuglevel", action="store", type=int, dest="debuglevel", help="Sets debug level")
     PARSER.add_argument("-is", "--inputstreams", action="store", dest="inputstreamdic", help="input streams filename dict")
     PARSER.add_argument("-i", "--inputfile", action="store", dest="inputfiledic", help="input filename dict")
     PARSER.add_argument("-o", "--outputfile", action="store", dest="outputfiledic", help="output filename dict")
     PARSER.add_argument("-e", "--expression", action="store", dest="expression", help="input expression")
-    PARSER.add_argument("-t", "--fname", action="store", dest="gname", help="Transformer name")
     PARSER.add_argument("-p", "--pipe", action="store_true", dest="pipemode", help="Pipe interaction mode")
+    PARSER.add_argument("transformer", help="Transformer name")
     ARGS = PARSER.parse_args()
     import sys
     if ((ARGS.outputfiledic and not ARGS.expression) and (ARGS.outputfiledic and not ARGS.inputfiledic)):
@@ -49,18 +48,17 @@ if __name__ == "__main__":
     logging.basicConfig(level = DEBUGLEVEL)
     from pydsl.Interaction.Translate import Translate
     MANAGER = Translate(ARGS)
-    if (ARGS.gname):
-        try: 
-            MANAGER.readTR(ARGS.gname)
-        except BadFileFormat:
-            print("Error reading input file")
-            sys.exit(1)
-        except KeyError as le:
-            print("Unable to load " + str(le))
-            sys.exit(1)
-    else:
-        print(TUSAGE)
-        sys.exit(0)
+    try: 
+        MANAGER.readTR(ARGS.transformer)
+    except BadFileFormat:
+        print("Error reading input file")
+        sys.exit(1)
+    #except KeyError as le:
+    #    print("Unable to load " + str(le))
+    #    sys.exit(1)
+    #else:
+    #    print(TUSAGE)
+    #    sys.exit(0)
     try:
         result = MANAGER.execute()
     except EOFError:
