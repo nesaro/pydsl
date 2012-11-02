53188f733eacb3c3675e1fa6c8a50a5285756d7f
Nestor Arocha <nesaro@gmail.com>
2012-11-02 15:14:02 +0000

    old diff code

---
+++ pydsl/Diff.py
@@ -0,0 +1,6 @@
+#Transformador teorico que obtiene las diferencias entre dos palabras aceptadas de una gramática.
+#A cada Grammar le pide el arbol u otra cosa para poder evaluar las diferencias
+
+inputdic = { "grammaname":"cstring", "value1":"cstring", "value2":"cstring"}
+outputdic = {"output":"cstring"} #TODO: Ver que gramatica se puede definir
+iclass = "PythonTransformer"
