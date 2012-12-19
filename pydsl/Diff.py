e701fbc2350a6eaa98ec7cc6ac93c0c3210bccb5
Nestor Arocha <nesaro@gmail.com>
2012-12-19 17:53:11 +0000

    removed old code, added license text

--- pydsl/Diff.py
+++ pydsl/Diff.py
@@ -1,9 +1,28 @@
-#Transformador teorico que obtiene las diferencias entre dos palabras aceptadas de una gramática.
-#A cada Grammar le pide el arbol u otra cosa para poder evaluar las diferencias
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#This file is part of pydsl.
+#
+#pydsl is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#pydsl is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with pydsl.  If not, see <http://www.gnu.org/licenses/>.
 
-inputdic = { "grammaname":"cstring", "value1":"cstring", "value2":"cstring"}
-outputdic = {"output":"cstring"} #TODO: Ver que gramatica se puede definir
-iclass = "PythonTransformer"
+
+__author__ = "Nestor Arocha"
+__copyright__ = "Copyright 2008-2012, Nestor Arocha"
+__email__ = "nesaro@gmail.com"
+
+import logging
+LOG = logging.getLogger(__name__)
+
 
 def diff(content1, content2, grammarlist = [], alphabetlist = []):
     result = {}
