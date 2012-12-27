9905dd5cf70dea1d3fa1d7fcf02485041741ec55
Nestor Arocha <nesaro@gmail.com>
2012-12-27 22:07:56 +0000

    fixed binary test

--- tests/test_Binaries.py
+++ tests/test_Binaries.py
@@ -24,6 +24,6 @@ class TestBinaries(unittest.TestCase):
     def testSearch(self):
         #self.assertEqual(os.system("python3 bin/search.py"),0)
         from subprocess import Popen,PIPE
-        p = Popen(['python3', 'bin/search.py'], stdout=PIPE)
+        p = Popen(['python3', 'bin/manager.py', "l"], stdout=PIPE)
         p.communicate()
         self.assertEqual(p.returncode, 0)
