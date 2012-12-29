71931a22341ddc048da669108c529eaa2b731bd5
Nestor Arocha <nesaro@gmail.com>
2012-12-29 10:12:12 +0000

    added Validate test

---
+++ tests/test_Validate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#This file is part of pydsl.
+#
+#pydsl is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#pydsl is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with pydsl.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test wrapper"""
+
+__author__ = "Nestor Arocha"
+__copyright__ = "Copyright 2008-2012, Nestor Arocha"
+__email__ = "nesaro@gmail.com"
+
+import unittest
+
+class TestValidate(unittest.TestCase):
+    def testBasic(self):
+        from pydsl.Validate import validate
+        from pydsl.Memory.Loader import load
+        grammar = load('date')
+        print(validate(grammar, "11/11/ab"))
