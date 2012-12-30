f6ba5a7c3e1b9ef4794b6a4416a81bbf84fa7835
Nestor Arocha <nesaro@gmail.com>
2012-12-30 17:27:13 +0000

    updated version and binary list

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
-import glob
-
 setup(name='pydsl',
-      version='0.1',
+      version='0.0.1',
       description='Python Domain Specific Language Tools',
       author='Nestor Arocha',
       author_email='nesaro@gmail.com',
       url='https://github.com/nesaro/pydsl',
       packages = find_packages(),
-      scripts=['bin/extract.py', 'bin/translate.py','bin/check.py','bin/search.py', 'bin/guess.py', 'bin/info.py', 'bin/validate.py', 'bin/parts.py'],
+      scripts=['bin/extract.py', 'bin/translate.py','bin/check.py', 'bin/manager.py', 'bin/guess.py', 'bin/validate.py', 'bin/parts.py'],
       install_requires=['ply'],
       package_data={'pydsl.contrib.grammar':['*.re', '*.bnf'] ,
           'pydsl.contrib.board':['*.board'],
-          'pydsl.contrib.dict':['*.dict'],},
+          'pydsl.contrib.dict':['*.dict'],
+          'pydsl.contrib.list':['*.py']}
      )
 
