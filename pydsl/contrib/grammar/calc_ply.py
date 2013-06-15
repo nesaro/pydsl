235a0b0c46e14ebd3d0622842c7dfe52b928bc03
Nestor Arocha <nesaro@gmail.com>
2013-06-15 19:35:15 +0100

    updated to the latest version

--- pydsl/contrib/grammar/calc_ply.py
+++ pydsl/contrib/grammar/calc_ply.py
@@ -1,23 +1,26 @@
 # -----------------------------------------------------------------------------
 # calc.py
 #
-# A simple calculator with variables.   This is from O'Reilly's
-# "Lex and Yacc", p. 63.
+# A simple calculator with variables -- all in one file.
 # -----------------------------------------------------------------------------
-
-#import sys
-#sys.path.insert(0,"../../../../")
 
 from pydsl.Exceptions import ParseError
 
 tokens = (
     'NAME','NUMBER',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
+    'LPAREN','RPAREN',
     )
-
-literals = ['=','+','-','*','/', '(',')']
 
 # Tokens
 
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_EQUALS  = r'='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
 t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
 def t_NUMBER(t):
@@ -29,82 +32,68 @@ def t_NUMBER(t):
         t.value = 0
     return t
 
+# Ignored characters
 t_ignore = " \t"
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
-    
+
 def t_error(t):
     raise ParseError("unknown character", t.lexpos)
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-    
-# Build the lexer
-#import ply.lex as lex
-#lex.lex()
 
 # Parsing rules
 
 precedence = (
-    ('left','+','-'),
-    ('left','*','/'),
+    ('left','PLUS','MINUS'),
+    ('left','TIMES','DIVIDE'),
     ('right','UMINUS'),
     )
 
 # dictionary of names
 names = { }
 
-def p_statement_assign(p):
-    'statement : NAME "=" expression'
-    names[p[1]] = p[3]
+def p_statement_assign(t):
+    'statement : NAME EQUALS expression'
+    names[t[1]] = t[3]
 
-def p_statement_expr(p):
+def p_statement_expr(t):
     'statement : expression'
-    #print(p[1])
-    pass
+    t[0] = t[1]
 
-def p_expression_binop(p):
-    '''expression : expression '+' expression
-                  | expression '-' expression
-                  | expression '*' expression
-                  | expression '/' expression'''
-    if p[2] == '+'  : p[0] = p[1] + p[3]
-    elif p[2] == '-': p[0] = p[1] - p[3]
-    elif p[2] == '*': p[0] = p[1] * p[3]
-    elif p[2] == '/': p[0] = p[1] / p[3]
+def p_expression_binop(t):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression TIMES expression
+                  | expression DIVIDE expression'''
+    if t[2] == '+'  : t[0] = t[1] + t[3]
+    elif t[2] == '-': t[0] = t[1] - t[3]
+    elif t[2] == '*': t[0] = t[1] * t[3]
+    elif t[2] == '/': t[0] = t[1] / t[3]
 
-def p_expression_uminus(p):
-    "expression : '-' expression %prec UMINUS"
-    p[0] = -p[2]
+def p_expression_uminus(t):
+    'expression : MINUS expression %prec UMINUS'
+    t[0] = -t[2]
 
-def p_expression_group(p):
-    "expression : '(' expression ')'"
-    p[0] = p[2]
+def p_expression_group(t):
+    'expression : LPAREN expression RPAREN'
+    t[0] = t[2]
 
-def p_expression_number(p):
-    "expression : NUMBER"
-    p[0] = p[1]
+def p_expression_number(t):
+    'expression : NUMBER'
+    t[0] = t[1]
 
-def p_expression_name(p):
-    "expression : NAME"
+def p_expression_name(t):
+    'expression : NAME'
     try:
-        p[0] = names[p[1]]
+        t[0] = names[t[1]]
     except LookupError:
-        print("Undefined name '%s'" % p[1])
-        p[0] = 0
+        raise ParseError("Undefined name",0)
+        t[0] = 0
 
-def p_error(p):
-    raise ParseError("unexpected character", p.value)
+def p_error(t):
+    raise ParseError("Syntax error at", t.value)
 
-#import ply.yacc as yacc
-#yacc.yacc()
-
-#while 1:
-#    try:
-#        s = raw_input('calc > ')
-#    except EOFError:
-#        break
-#    if not s: continue
-#    yacc.parse(s)
 iclass="PLY"
