5166defeb8b1d8145efd2fe819aeda0cf4233cf7
Nestor Arocha <nesaro@gmail.com>
2013-10-02 10:00:53 +0100

    removed Antlr grammar experiments

--- tests/unit/test_BNFLoad.py
+++ tests/unit/test_BNFLoad.py
@@ -7,12 +7,6 @@
 
 import unittest
 
-class TestAntlr(unittest.TestCase):
-    """Loading a bnf instance from a .g file"""
-    def testFileLoader(self):
-        from pydsl.Memory.File.Antlr import  load_anltr_file
-        self.assertTrue(load_anltr_file("tests/FOL.g"))
-
 class TestPydsl(unittest.TestCase):
     """Loading a bnf instance from a .bnf file"""
     def testFileLoader(self):
