57d09c6e306e6ef9666a1b3fa878c00e62926656
Nestor Arocha <nesaro@gmail.com>
2013-10-12 10:32:35 +0100

    partial work llkrecursive descent

--- pydsl/Parser/RecursiveDescent.py
+++ pydsl/Parser/RecursiveDescent.py
@@ -310,3 +310,54 @@ class LL1RecursiveDescentParser(TopDownParser):
             raise Exception("Not matched")
 
 
+class LLkRecursiveDescentParser(TopDownParser):
+    def get_trees(self, data, lookahead=1, showerrors = False): # -> list:
+        """ returns a list of trees with valid guesses """
+        if showerrors:
+            raise NotImplementedError("This parser doesn't implement errors")
+        self.data = data
+        self.index = 0
+        self.lookahead=lookahead
+        return [self.__aux_parser(self._productionset.initialsymbol)]
+
+    def __aux_parser(self, symbol):
+        from pydsl.Grammar.Symbol import TerminalSymbol
+        if isinstance(symbol, TerminalSymbol):
+            LOG.debug("matching symbol %s, data:%s, index:%s" % (symbol,self.data,self.index ))
+            result= self.match(symbol)
+            LOG.debug("symbol matched %s" % result)
+            return result
+        productions = self._productionset.getProductionsBySide(symbol)
+        first_of_each_production = {}
+        for production in productions:
+            first_of_each_production[production] = [x for x in self._productionset.first_lookup(production.rightside[0], self.lookahead)]
+        valid_firsts = [production_instance for (production_instance,first_set) in first_of_each_production.items() if check(first_set, self.current)]
+        if len(valid_firsts) != 1:
+            raise Exception("Expected only one valid production, found %s" % len(valid_firsts))
+        childlist = []
+        for element in valid_firsts[0].rightside:
+            childlist.append(self.__aux_parser(element))
+        left = childlist[0].leftpos
+        right = childlist[-1].rightpos
+        content = [x.content for x in childlist]
+        return ParseTree(left, right, [symbol], content, valid_firsts[0], childlist=childlist)
+
+
+    def consume(self):
+        self.index +=1
+        if self.index > len(self.data):
+            raise IndexError("Attempted to consume index %s of data %s" % (self.index, self.data))
+
+    @property
+    def current(self):
+        result = self.data[self.index]
+        return result
+
+    def match(self, symbol):
+        if symbol.check(self.current):
+            current = self.current
+            self.consume()
+            return ParseTree(self.index-1, self.index, [symbol], current, None)
+        else:
+            raise Exception("Not matched")
+
