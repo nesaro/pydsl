90c051036466e6fd7bbacbdb5cc715637e2cf485
Nestor Arocha <nesaro@gmail.com>
2014-01-05 11:06:41 +0000

    adds iteration limit

--- pydsl/Tree.py
+++ pydsl/Tree.py
@@ -95,15 +95,19 @@ class Sequence:
         """Returns list"""
         valid_sets = [[x] for x in self.possible_items if x['left'] == 0]
         change = True
-        while change:
+        niter = 200
+        while change and niter > 0:
             change = False
+            niter -=1
             for possible in self.possible_items:
                 for current_valid in valid_sets[:]:
                     if possible['left'] == current_valid[-1]['right']:
                         if current_valid + [possible] not in valid_sets:
-                            if possible['content'] != current_valid[-1]['content']:
+                            if current_valid[-1]['left'] != current_valid[-1]['right'] or possible['left'] != possible['right']: #avoids Null insertion twice
                                 valid_sets.append(current_valid + [possible])
                                 change = True
+        if not niter:
+            raise Exception('too many iterations')
         return valid_sets
 
     def right_limit_list(self):
