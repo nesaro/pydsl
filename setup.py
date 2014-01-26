5891f090a6ca19d81af753a7c8a31eb9da0ead5f
Ptolom <ptolom@hexifact.co.uk>
2014-01-26 19:32:58 +0000

    Added .parsley to setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='pydsl',
       packages = find_packages(exclude=['tests.*']),
       install_requires=['ply'],
       package_dir={'pydsl.contrib': 'pydsl/contrib'},
-      package_data={'pydsl.contrib': ['grammar/*.re','grammar/*.bnf','dict/*.dict']},
+      package_data={'pydsl.contrib': ['grammar/*.re','grammar/*.bnf','grammar/*.parsley','dict/*.dict']},
       classifiers=[
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Programming Language :: Python :: 3',
